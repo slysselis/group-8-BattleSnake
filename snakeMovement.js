a52407d5680b94ab4e49581ad0cbae3195b646dd
dskenderoglou <146946788+dskenderoglou@users.noreply.github.com>
2025-04-26 15:54:10 +0300

    Feature: snake now moves to food

--- snakeMovement.js
+++ snakeMovement.js
@@ -27,3 +27,17 @@ function move(gameState) {
   } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
     isMoveSafe.up = false;
   }
+
+  // Move towards the closest food if possible
+  if (food.length > 0) {
+    let closestFood = food.reduce((closest, current) => {
+      let closestDist = Math.abs(closest.x - myHead.x) + Math.abs(closest.y - myHead.y);
+      let currentDist = Math.abs(current.x - myHead.x) + Math.abs(current.y - myHead.y);
+      return currentDist < closestDist ? current : closest;
+    });
+
+    if (closestFood.x < myHead.x && isMoveSafe.left) return { move: "left" };
+    if (closestFood.x > myHead.x && isMoveSafe.right) return { move: "right" };
+    if (closestFood.y < myHead.y && isMoveSafe.down) return { move: "down" };
+    if (closestFood.y > myHead.y && isMoveSafe.up) return { move: "up" };
+  }
