e7143147f2cb8b1af7cb949794bb9424e8b95097
gsarakotsis <147416279+gsarakotsis@users.noreply.github.com>
2025-05-17 18:40:03 +0300

    Added imported function to index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ import preventSnakeCollision from './preventSnakeCollision.js';
 import preventBackwardsMovement from './preventBackwardsMovement.js';
 import moveTowardFood from './moveTowardFood.js';
 import headToHead from './headToHead.js';
+import allowTailMovement from './allowTailMovement.js';
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
 // TIP: If you open your Battlesnake URL in a browser you should see this data
