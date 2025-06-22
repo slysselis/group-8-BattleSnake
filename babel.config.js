4a9a1be99e1adcbacf93164ded1c3fc11217fedb
slysselis <147416195+slysselis@users.noreply.github.com>
2025-06-22 18:11:43 +0300

    Fix/babel config
    Fix babel config modules

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-// Babel configuration for transpiling modern JavaScript to compatible versions
-export default {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }]], // Use preset-env to target current Node.js version
-};
